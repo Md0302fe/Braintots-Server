b715d56fa2ff92be9d8ee685c8e29d98c6ea6259
Md0302fe <minhduc2003kols@gmail.com>
2024-11-02 17:04:03 +0700

    new update 2/11/2024

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
-    // id product - extra
-    // origin
     name: { type: String, required: true },
     masanpham: { type: String, required: true, unique: true },
     image: { type: String, required: true },
@@ -15,14 +13,22 @@ const productSchema = new mongoose.Schema(
     price: { type: Number, required: true },
     countInStock: { type: Number, required: true },
     rating: { type: Number, required: true },
-    discount: { type: Number },
-    selled: { type: Number },
+    discount: { type: Number, default: 0 },
+    selled: { type: Number, default: 0 },
     description: { type: String },
   },
   {
     timestamps: true,
+    // turn on virtual fields
+    toJSON: {virtuals:true}, // Bật virtual fields khi chuyển đổi thành JSON
+    toObject: { virtuals: true }, // Bật virtual fields khi chuyển đổi thành object
   }
 );
+
+// Virtual field to calculated price after discount
+productSchema.virtual("priceAfterDiscount").get(function() {
+  return this.price - ((this.price * this.discount) / 100);
+});
 
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
